751c139079baa3ebff1626cf8381daf05fdc27b4
Ganesh Giri <giri.ganesh337@gmail.com>
2021-07-11 15:54:45 +0530

    initial project add

---
+++ src/main/java/com/ganesh/microserviceguru/MicroserviceGuruApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.microserviceguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceGuruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ganesh/microserviceguru/MicroserviceGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganesh.microserviceguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGuruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
